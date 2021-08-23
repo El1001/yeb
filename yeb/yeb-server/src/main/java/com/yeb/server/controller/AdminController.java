818b40f5386ecdabd9e05aba0d1992fcbb5e0d12
unknown <253650279@qq.com>
2021-08-23 15:57:37 +0800

    个人中心

--- yeb/yeb-server/src/main/java/com/yeb/server/controller/AdminController.java
+++ yeb/yeb-server/src/main/java/com/yeb/server/controller/AdminController.java
@@ -9,9 +9,13 @@ import com.yeb.server.service.IRoleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -65,5 +69,29 @@ public class AdminController {
     @PutMapping("/role")
     public RespBean updateAdminRole(Integer adminId, Integer[] rids) {
         return adminService.updateAdminRole(adminId, rids);
+
+    }
+
+    @ApiOperation(value = "更新当前用户信息")
+    @PutMapping("/admin/info")
+    public RespBean updateAdmin(@RequestBody Admin admin, Authentication authentication) {
+//更新成功，重新构建Authentication对象
+        if (adminService.updateById(admin)) {
+            SecurityContextHolder.getContext().setAuthentication(new
+                    UsernamePasswordAuthenticationToken(admin,
+                    authentication.getCredentials(),
+                    authentication.getAuthorities()));
+            return RespBean.success("更新成功!");
+        }
+        return RespBean.error("更新失败!");
+    }
+
+    @ApiOperation(value = "更新用户密码")
+    @PutMapping("/admin/pass")
+    public RespBean updateAdminPassword(@RequestBody Map<String, Object> info) {
+        String oldPass = (String) info.get("oldPass");
+        String pass = (String) info.get("pass");
+        Integer adminId = (Integer) info.get("adminId");
+        return adminService.updatePassword(oldPass, pass, adminId);
     }
 }
